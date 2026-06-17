21eba813a24cb00cd2bf23ccb14465b0248002b9
Admin <wlsql852@naver.com>
2026-06-17 15:30:54 +0900

    commit init

---
+++ src/main/java/io/echo/silentcabin/SilentCabinApplication.java
@@ -0,0 +1,13 @@
+package io.echo.silentcabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilentCabinApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SilentCabinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/echo/silentcabin/SilentCabinApplicationTests.java
@@ -0,0 +1,13 @@
+package io.echo.silentcabin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilentCabinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
