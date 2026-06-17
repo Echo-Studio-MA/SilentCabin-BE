c9f2af2e9d03e888da985e098345a2797f1df3bb
Admin <wlsql852@naver.com>
2026-06-17 16:20:47 +0900

    [CHORE] test에 h2 설정

--- src/test/java/io/echo/silentcabin/SilentCabinApplicationTests.java
+++ src/test/java/io/echo/silentcabin/SilentCabinApplicationTests.java
@@ -2,8 +2,10 @@ package io.echo.silentcabin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SilentCabinApplicationTests {
 
     @Test
